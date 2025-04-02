55ca1fd4fc10fe794a27c3b3a81f0e1e0b6b934d
Varun Sharma <varun05772@gmail.com>
2025-04-02 12:10:35 +0530

    removed use topology

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const PORT = 8001;
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
   })
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
